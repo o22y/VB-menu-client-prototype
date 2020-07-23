0f65be7c5bb5aa2af7c09935c89b7578ac06b8ec
o22y <46899676+o22y@users.noreply.github.com>
2020-07-24 00:24:13 +0300

    Download menu prototype build

---
+++ precache-manifest.bc5e3786e70cf8d9a9aa923c450e7130.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5a196915371744da3b84dcc01ae044",
+    "url": "/VB-menu-prototype/index.html"
+  },
+  {
+    "revision": "39a4d02453af2fa11214",
+    "url": "/VB-menu-prototype/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "503803d71db2ecc06ce0",
+    "url": "/VB-menu-prototype/static/css/main.c25723c6.chunk.css"
+  },
+  {
+    "revision": "39a4d02453af2fa11214",
+    "url": "/VB-menu-prototype/static/js/2.42bbf764.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/VB-menu-prototype/static/js/2.42bbf764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503803d71db2ecc06ce0",
+    "url": "/VB-menu-prototype/static/js/main.59106642.chunk.js"
+  },
+  {
+    "revision": "8bba8b386b1be4d67022",
+    "url": "/VB-menu-prototype/static/js/runtime-main.83591d5b.js"
+  },
+  {
+    "revision": "b6839538343b0b7b63654aeb7a950229",
+    "url": "/VB-menu-prototype/static/media/rest.b6839538.jpg"
+  },
+  {
+    "revision": "e024b16e0d34bd9a1b2926576208ee09",
+    "url": "/VB-menu-prototype/static/media/stripses.e024b16e.jpg"
+  }
+]);
